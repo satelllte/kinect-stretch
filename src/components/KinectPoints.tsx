eba941b4403e6189306f377c2603ee23cfaf4fb7
Yehor <20585619+satelllte@users.noreply.github.com>
2026-09-21 11:38:10 +0300

    kinect: brightnessThreshold

--- src/components/KinectPoints.tsx
+++ src/components/KinectPoints.tsx
@@ -18,14 +18,21 @@ const kinectVertexShader = /* glsl */ `
 
   const float XtoZ = 1.11146;
   const float YtoZ = 0.83359;
+  const float brightnessThreshold = 0.8;
 
   void main() {
     vUv = vec2(position.x / width, position.y / height);
 
     vec4 color = texture2D(map, vUv);
-    float depth = (color.r + color.b) / 1.5;
+    float brightness = (color.r + color.g + color.b) / 3.0;
+    if (brightness > brightnessThreshold) {
+      // put this point outside the clipping range
+      gl_Position = vec4(2.0, 2.0, 2.0, 1.0);
+      gl_PointSize = 0.0;
+      return;
+    }
 
-    float z = (1.0 - depth) * (farClipping - nearClipping) + nearClipping;
+    float z = (1.0 - brightness) * (farClipping - nearClipping) + nearClipping;
 
     vec4 pos = vec4(
       (position.x / width - 0.5) * z * XtoZ,
